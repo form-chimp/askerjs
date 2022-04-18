5a65815d6a7ffd196dff50fdd73ddfe499c85d4b
Anas Mohamed <amohamed2479@gmail.com>
2022-04-18 14:52:08 -0500

    update test

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,42 @@
-import Asker from "../src/Asker";
+import Asker from "../lib/build";
 
 
 let target = document.getElementById('AskerJS')
 
-new Asker(target)
+
+let questions = {
+    'q-1':{
+        text: 'Isn\'t this pretty cool?',
+        type: 'singleChoice',
+        required: false,
+        choices: [
+            '👏 Yes',
+            '👎 No'
+        ],
+        next: 'q-2'
+    },
+    'q-2':{
+        text: 'Would you like to see more?',
+        type: 'singleChoice',
+        choices: [
+            '🙄 Heck yeah',
+            '👎 Nah bro' 
+        ],
+        required: false,
+    }
+}
+
+
+
+let asker = new Asker(target, questions, formComplete);
+
+asker.ask(questions["q-1"]);
+
+function formComplete(data){
+    
+    asker.newContent(`
+
+        <h1 class="text-5xl">🎉🎉 Thank you!!</h1>
+    
+    `);
+}
