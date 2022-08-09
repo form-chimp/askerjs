c5bc14b974b036e4d23df7d0cbbab08475e38202
anas-mohamedzz <49694746+anas2479@users.noreply.github.com>
2022-08-08 22:20:35 -0500

    remove un used param

--- src/Asker.js
+++ src/Asker.js
@@ -19,8 +19,7 @@ export default class Asker {
      * @param {HTMLElement} target The target element to render the form to.
      * @param {Object} questions Questions object
      * @param {Function} onComplete Function to be called when the form is complete.  
-     * @param {Object} options
-     *  
+     * @param {Object} options Config options for Asker.
      */
     constructor(target,questions,onComplete, options){
 
